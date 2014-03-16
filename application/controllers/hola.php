730ebf99e3ee97695ca979d3274c921d6e2953f1
Eliecer David <eliecer612@hotmail.com>
2014-03-16 13:36:18 -0500

    aprendiendo a usar un controlador con CI(saludos)

---
+++ application/controllers/hola.php
@@ -0,0 +1,35 @@
+<?php 
+class Hola extends CI_Controller {
+	
+	function index()
+	{
+		echo "hola mundo";
+	}
+
+	function saludo()
+	{
+		echo "Clase1_CodeIgniter";
+	}
+
+	function saludo1($id=0)
+	{
+		switch ($id) {
+			case '0':
+				echo "saludo 0";
+				break;
+
+			case '1':
+				echo "saludo 1";
+				break;
+
+			case '2':
+				echo "saludo 2";
+				break;
+
+			default:
+				echo "saludo no definido";
+				break;
+		}
+	}
+}
+?>
